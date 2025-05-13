3d029b89001858603157bb5cadab06e7787fe4bf
Sheketli <ST10452756@rcconnect.edu.za>
2025-05-13 12:34:42 +0000

    removal of ResponseDelegate method

--- OusesCybersecurityAwarenessBotPOE/UserMemory.cs
+++ OusesCybersecurityAwarenessBotPOE/UserMemory.cs
@@ -6,8 +6,6 @@ using System.Threading.Tasks;
 
 namespace OusesCybersecurityAwarenessBotPOE
 {
-    public delegate string ResponseDelegate(string input);
-
     internal class UserMemory
     {
         public string Name { get; set; }
