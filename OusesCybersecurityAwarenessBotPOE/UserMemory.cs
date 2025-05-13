097cd31632d7bf48fbcd30dd07aca9511251661a
Sheketli <ST10452756@rcconnect.edu.za>
2025-05-13 12:18:48 +0000

    User Memory (get and set)

---
+++ OusesCybersecurityAwarenessBotPOE/UserMemory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OusesCybersecurityAwarenessBotPOE
+{
+    internal class UserMemory
+    {
+        public string Name { get; set; }
+        public string FavoriteTopic { get; set; }
+    }
+}
