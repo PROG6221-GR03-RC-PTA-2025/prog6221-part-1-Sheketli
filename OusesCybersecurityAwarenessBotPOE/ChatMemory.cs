866ed847cc8341bf4c4d8594584fbf0c8c1856e7
Sheketli <ST10452756@rcconnect.edu.za>
2025-05-14 13:29:44 +0000

    Dictionary to store user memory

---
+++ OusesCybersecurityAwarenessBotPOE/ChatMemory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OusesCybersecurityAwarenessBotPOE
+{
+    internal class ChatMemory
+    {
+        private Dictionary<string, string> memory = new Dictionary<string, string>();
+
+        public void Remember(string key, string value)
+        {
+            memory[key] = value;
+        }
+
+        public string Recall(string key)
+        {
+            return memory.ContainsKey(key) ? memory[key] : null;
+        }
+
+        public bool HasMemory(string key)
+        {
+            return memory.ContainsKey(key);
+        }
+    }
+}
